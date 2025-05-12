25fa7331e04b0899f21801fcb50c13c292bc39b8
paulo <pauloulisses1212@gmail.com>
2025-05-12 12:23:32 -0300

    modulo 02 do curso

---
+++ MOD-02/operadores.js
@@ -0,0 +1,9 @@
+// OPERADOR TERNÁRIO
+
+const idade = 18;
+const mensagem = idade >= 18 ? "Você é maior de idade." : "Você é menor de idade.";
+console.log(mensagem); // Saída: "Você é maior de idade."
+
+// Se for verdadeira, retorna o valor após o ?.
+
+// Se for falsa, retorna o valor após o :.
